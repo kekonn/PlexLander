73bd7450bbbc0726b88e204b3de4e21eedf25f76
bloodsplatter <ketsukonn@gmail.com>
2017-03-24 22:03:37 +0100

    Set up the apps on the landing page

--- PlexLander/Controllers/LandingController.cs
+++ PlexLander/Controllers/LandingController.cs
@@ -5,15 +5,24 @@ using System.Threading.Tasks;
 using PlexLander.ViewModels.Landing;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using PlexLander.Data;
+using PlexLander.Models;
 
 namespace PlexLander.Controllers
 {
-    public class LandingController : PlexLanderBaseController
+    public class LandingController : Controller
     {
+        private readonly PlexLanderContext _context;
+
+        public LandingController(PlexLanderContext context)
+        {
+            _context = context;
+        }
 
         public IActionResult Index()
         {
-            return View();
+            var viewModel = new LandingViewModel() { AppList = new List<App>(_context.Apps.AsEnumerable()) };
+            return View(viewModel);
         }
 
 
--- PlexLander/ViewModels/Landing/LandingViewModel.cs
+++ PlexLander/ViewModels/Landing/LandingViewModel.cs
@@ -2,28 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PlexLander.Models;
 
 namespace PlexLander.ViewModels.Landing
 {
     public class LandingViewModel
     {
-        public IEnumerable<LandingApp> AppList { get; set; }
-    }
-
-    public class LandingApp
-    {
-        /// <summary>
-        /// The url we have to call 
-        /// </summary>
-        public string Url { get; set; }
-        /// <summary>
-        /// a local url to an image that serves as an icon.
-        /// </summary>
-        public string Icon { get; set; }
-
-        /// <summary>
-        /// The display name that is used on the website
-        /// </summary>
-        public string Name { get; set; }
+        public ICollection<App> AppList { get; set; }
     }
 }
