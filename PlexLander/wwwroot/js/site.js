57fb8f2542d328e9668e9436198af8c97d09e170
bloodsplatter <ketsukonn@gmail.com>
2017-03-27 21:16:38 +0200

    more work on edit

--- PlexLander/wwwroot/js/site.js
+++ PlexLander/wwwroot/js/site.js
@@ -5,3 +5,18 @@
     $(modalElement).modal("show");
     return false;
 };
+
+function editApp(appId, editButton) {
+    var viewClass = ".view-app-" + appId;
+    var editClass = ".edit-app-" + appId;
+    $(viewClass).fadeOut("fast");
+    $(editClass).fadeIn("fast");
+};
+
+function cancelEditApp(appId) {
+    var viewClass = ".view-app-" + appId;
+    var editClass = ".edit-app-" + appId;
+    $(editClass).fadeOut("fast");
+    //TODO clean up input fields
+    $(viewClass).fadeIn("fast");
+};
