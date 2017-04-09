27b6edd29071bbee340cc3c6849772dac39734f5
bloodsplatter <ketsukonn@gmail.com>
2017-04-09 21:09:48 +0200

    added a rest framework for testing.

---
+++ PlexLander/Plex/PlexServer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlexLander.Plex
+{
+    public class PlexServer
+    {
+        public string ApiEndpoint { get; set; }
+        public string Token { get; set; }
+    }
+}
